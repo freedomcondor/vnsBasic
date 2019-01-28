0551d2dc6e70d79ac60a3f3981b92f38e35f4276
Condor3397 <freedomcondor@126.com>
2019-01-28 17:14:32 +0100

    try to add a experiment0 the ultimate one

---
+++ quadcopter0.lua
@@ -0,0 +1,433 @@
+------------------------------------------------------------------------
+--   Global Variables
+------------------------------------------------------------------------
+
+require("PackageInterface")
+local Vec3 = require("math/Vector3")
+
+--require("debugger")
+
+local parentIndex = {
+	quadcopter0 = nil,
+	quadcopter1 = "quadcopter0",
+	quadcopter2 = "quadcopter0",
+	quadcopter3 = "quadcopter1",
+}
+local transParaIndex = {}
+
+local childRobots = {} -- recruitedVehicles["iamid"] = true
+
+------------------------------------------------------------------------
+--   ARGoS Functions
+------------------------------------------------------------------------
+function init()
+	reset()
+end
+
+-------------------------------------------------------------------
+function step()
+	-- detect boxes into boxesVT[i]
+	-- local boxPos_vt = getBoxesVT()	
+	local boxesVT = getBoxesVT()	
+		-- V means vector = {x,y}
+
+	-- detect robots into robotsRT[i] and robotsRT[id]
+	local robotsRT = getRobotsRT()
+		-- R means robot = {locV, dirN, idS, parent}
+
+	-- receive robots from other quadcopter
+	local cmdListCT = getCMDListCT()  
+		-- CT means CMD Table(array)
+		-- a cmd contains: {cmdS, fromIDS, dataNST}
+	for i, cmdC in ipairs(cmdListCT) do
+		if cmdC.cmdS == "robotsInfo" then
+			local receivedRobotsRT = bindRobotInfoDataRT(cmdC.dataNST)
+			robotsRT, transParaIndex[cmdC.fromIDS] = 
+				joinReceivedRobotsRT(robotsRT, receivedRobotsRT)
+		end
+	end
+
+	-- send robots to parent quadcopter 
+	if parentIndex[getSelfIDS()] ~= nil then
+		setVelocity(0,0,1)
+		local robotsDataNST = makeRobotInfoDataNST(robotsRT) -- NST means a table of number or string
+		sendCMD(parentIndex[getSelfIDS()], "robotsInfo", robotsDataNST)
+	end
+
+	if parentIndex[getSelfIDS()] == nil then
+		--[[
+		print("I am parent, I got:")
+		for i, v in ipairs(robotsRT) do
+			print("\t", i, v.idS, v.locV.x, v.locV.y, v.dirN)
+		end
+		--]]
+
+		for i, vehicleR in ipairs(robotsRT) do
+			-- TODO: check lost vehicle
+			if childRobots[vehicleR.idS] == nil then
+				-- a free vehicle
+				local targetBoxV = getTargetBoxV(vehicleR.locV, boxesVT)
+				if targetBoxV ~= nil then
+					sendCMD(vehicleR.idS, "recruit")
+					childRobots[vehicleR.idS] = true
+					print(getSelfIDS(), ": i recruit vehicle", vehicleR.idS)
+				end
+			else
+				-- a holding vehicle
+				local targetBoxV, boxDir = getTargetBoxV(vehicleR.locV, boxesVT)
+				if targetBoxV ~= nil then
+					local boxDirtoRobotN = getBoxDirtoRobot(vehicleR.locV, targetBoxV)
+					local centerDirtoRobotN = getBoxDirtoRobot(vehicleR.locV, {x=0,y=0})
+	
+					-- calc the box is left or right to the robot it self
+					local difN = boxDirtoRobotN - vehicleR.dirN
+					while difN > 180 do
+						difN = difN - 360
+					end
+		
+					while difN < -180 do
+						difN = difN + 360
+					end
+					local baseSpeedN = 7
+					if difN > 10 or difN < -10 then
+						if (difN > 0) then
+							setRobotVelocity(vehicleR.idS, -baseSpeedN, baseSpeedN)
+						else
+							setRobotVelocity(vehicleR.idS, baseSpeedN, -baseSpeedN)
+						end
+					else
+						setRobotVelocity(vehicleR.idS, baseSpeedN, baseSpeedN)
+					end
+				else
+					-- no box to move, 
+					--if boxDir == nil then
+						-- box in position, dismiss robot
+						print(getSelfIDS(), ": i dismiss vehicle", vehicleR.idS)
+						sendCMD(vehicleR.idS, "dismiss")
+						childRobots[vehicleR.idS] = nil
+					--else
+					--	print("I send a turn cmd")
+					--	sendCMD(vehicleR.idS, "turn", boxDir) -- boxDir is 1(left) or 2(right)
+					--end
+				end
+			end -- end of if childRobots
+		end -- end of for vehicle
+	
+	end
+end
+
+-------------------------------------------------------------------
+function reset()
+end
+
+-------------------------------------------------------------------
+function destroy()
+	-- put your code here
+end
+
+------------------------------------------------------------------------
+--   Customize Functions
+------------------------------------------------------------------------
+function setRobotVelocity(id, x,y)
+	sendCMD(id, "setspeed", {x,y})
+	--local bytes = tableToBytes(id, getSelfIDS(), "setspeed", {x,y})
+	--robot.radios["radio_0"].tx_data(bytes)
+end
+
+--[[
+-------------------------------------------------------------------
+function getCMD()
+	for i, rxBytesBT in pairs(getReceivedDataTableBT()) do	-- byte table
+		local toIDS, fromIDS, cmdS, rxNumbersNT = bytesToTable(rxBytesBT)
+		if toIDS == getSelfIDS() then
+			return fromIDS, cmdS, rxNumbersNT
+		end
+	end
+end
+
+function sendCMD(toidS, cmdS, txDataNT)
+	local txBytesBT = tableToBytes(toidS, 
+	                               getSelfIDS(), 
+	                               cmdS,
+	                               txDataNT)
+	transData(txBytesBT)
+end
+--]]
+
+-------------------------------------------------------------------
+-- get boxes
+-------------------------------------------------------------------
+function getBoxesVT()
+	local boxesVT = {}   -- vt for vector, which a table = {x,y}
+	for i, detectionT in ipairs(getLEDsT()) do	
+		-- a detection is a complicated table
+		-- containing location and color information
+		boxesVT[i] = getBoxPosition(detectionT)
+	end	
+	return boxesVT
+end
+
+function getBoxPosition (detection)
+	local pos = {}
+	pos.x = detection.center.x - 320
+	pos.y = detection.center.y - 240
+	pos.y = -pos.y 				-- make it left handed coordination system
+	return pos
+end
+
+-------------------------------------------------------------------
+-- get robots
+-------------------------------------------------------------------
+function getRobotsRT()
+	robotsRT = {}
+	for i, tagDetectionT in pairs(getTagsT()) do
+		-- a tag detection is a complicated table
+		-- containing center, corners, payloads
+		
+		-- get robot info
+		local locV, dirN, idS = getRobotInfo(tagDetectionT)
+			-- locV V for vector {x,y}
+			-- loc (0,0) in the middle, x+ right, y+ up , 
+			-- dir from 0 to 360, x+ as 0
+
+		robotsRT[i] = {locV = locV, 
+		               dirN = dirN, 
+		               idS = idS, 
+		               parent = getSelfIDS(),
+		              }
+		robotsRT[idS] = robotsRT[i]
+	end
+	return robotsRT
+end
+
+function getRobotInfo(tagT)
+	-- a tag is a complicated table with center, corner, payload
+	local degN = calcRobotDir(tagT.corners)
+		-- a direction is a number from 0 to 360, 
+		-- with 0 as the x+ axis of the quadcopter
+		
+	local locV = {}
+	locV.x = tagT.center.x - 320
+	locV.y = tagT.center.y - 240
+	locV.y = -locV.y 				-- make it left handed coordination system
+
+	local idS = tagT.payload
+
+	return locV, degN, idS
+end
+
+function makeRobotInfoDataNST(robotsRT)
+	-- robotsRT is a table of {locV, dirN, idS}
+	local dataNST = {}
+	dataNST[1] = #robotsRT	-- a table of number or String
+	local i = 2
+	for _, v in ipairs(robotsRT) do
+		dataNST[i] = v.idS
+		dataNST[i+1] = v.locV.x
+		dataNST[i+2] = v.locV.y
+		dataNST[i+3] = v.dirN
+		dataNST[i+4] = v.parent
+		i = i + 5
+	end
+	return dataNST
+end
+
+function bindRobotInfoDataRT(dataNST)
+	local n = dataNST[1]
+	local robotsRT = {}
+	local i = 2
+	for j = 1, n do
+		robotsRT[j] = {}
+		robotsRT[j].idS = dataNST[i]
+		robotsRT[j].locV = {x = dataNST[i+1],
+		                    y = dataNST[i+2]}
+		robotsRT[j].dirN = dataNST[i+3]
+		robotsRT[j].parent = dataNST[i+4]
+		i = i + 5
+	end
+	return robotsRT
+end
+-------------------------------------------------------------------
+-- calculations
+-------------------------------------------------------------------
+function getTargetBoxV(robotLocV, boxesVT)
+-- this function finds a box from boxes array for a robot to push based on:
+-- if the box is outside the center zone (5000 length)
+-- if the angle between the robot to the center (0,0)
+--              and     the robot to the box
+--        is small enough (cos >0.9)
+-- if the box is towards inside not outside
+-- then return the box location
+-- if no such box, return nil
+
+	--TODO: find the nearest one
+	local disR = robotLocV.x * robotLocV.x +
+	             robotLocV.y * robotLocV.y
+	for i, boxV in ipairs(boxesVT) do
+		local disBoxtoCenterN = boxV.x * boxV.x + boxV.y * boxV.y
+		if disBoxtoCenterN > 5000 then
+			-- relative is the vector from robot to box
+			local relativeV = {x = boxV.x - robotLocV.x,
+			                   y = boxV.y - robotLocV.y,}
+			local lRelN = math.sqrt(relativeV.x * relativeV.x +
+			                        relativeV.y * relativeV.y )
+			local lRobN = math.sqrt(robotLocV.x * robotLocV.x +
+			                        robotLocV.y * robotLocV.y )
+			local cosN = (-robotLocV.x * relativeV.x - robotLocV.y * relativeV.y) / 
+			             lRelN / lRobN
+			if cosN > 0.9 and lRobN > lRelN then
+				print(i,"box out and with right direction")
+				return boxV, nil
+			end
+		else
+			print(i,"box in position")
+			return nil, nil
+		end
+	end
+	for i, boxV in ipairs(boxesVT) do
+		-- relative is the vector from robot to box
+		local relativeV = {x = boxV.x - robotLocV.x,
+		                   y = boxV.y - robotLocV.y,}
+		local lRelN = math.sqrt(relativeV.x * relativeV.x +
+		                        relativeV.y * relativeV.y )
+		local relativeV3 = Vec3:create(relativeV.x, relativeV.y, 0)
+		local centerV3 = Vec3:create(-robotLocV.x, -robotLocV.y, 0)
+		local dirV3 = relativeV3 * centerV3
+		local dirN = 1
+		if dirV3.z > 0 then -- box is on the right
+			dirN = 2
+		end
+		if lRelN < 50 then
+			print(i,"test1")
+			return nil, dirN	-- TODO: check left or right
+		end
+	end
+	print(i,"test2")
+	return nil, nil
+end
+
+-------------------------------------------------------------------
+function calcRobotDir(corners)
+	-- a direction is a number from 0 to 360, 
+	-- with 0 as the x+ axis of the quadcopter
+	local front = {}
+	front.x = (corners[3].x + corners[4].x) / 2
+	front.y = -(corners[3].y + corners[4].y) / 2
+	local back = {}
+	back.x = (corners[1].x + corners[2].x) / 2
+	back.y = -(corners[1].y + corners[2].y) / 2
+	local deg = calcDir(back, front)
+	return deg
+end
+
+-------------------------------------------------------------------
+function getRobotHead(tag)
+	-- a direction is a number from 0 to 360, 
+	-- with 0 as the x+ axis of the quadcopter
+	local pos = {}
+	pos.x = ((tag.corners[3].x + tag.corners[4].x) / 2) - 320
+	pos.y = ((tag.corners[3].y + tag.corners[4].y) / 2) - 240
+	pos.y = -pos.y 				-- make it left handed coordination system
+	return pos
+end
+
+-------------------------------------------------------------------
+function getBoxDirtoRobot(robotPos, boxPos)
+	-- a direction is a number from 0 to 360, 
+	-- with 0 as the x+ axis of the quadcopter
+	local deg = calcDir(robotPos, boxPos)
+	return deg
+end
+
+-------------------------------------------------------------------
+function calcDir(center, target)
+	-- from center{x,y} to target{x,y} in left hand
+	-- calculate a deg from 0 to 360, x+ is 0
+	local x = target.x - center.x
+	local y = target.y - center.y
+	local deg = math.atan(y / x) * 180 / 3.1415926
+	if x < 0 then
+		deg = deg + 180
+	end
+	if deg < 0 then
+		deg = deg + 360
+	end
+	return deg
+end
+
+--------------------------------------------------------------------
+function joinReceivedRobotsRT(robotsRT, receivedRobotsRT)
+	-- find a common robot
+	local paraT = {} -- rotation and translation parameters
+	for i, vR in ipairs(receivedRobotsRT) do
+		if robotsRT[vR.idS] ~= nil then
+			local thN = robotsRT[vR.idS].dirN - vR.dirN
+			local thRadN = thN * math.pi / 180
+			paraT.x = vR.locV.x * math.cos(thRadN) - 
+			          vR.locV.y * math.sin(thRadN)
+			paraT.y = vR.locV.x * math.sin(thRadN) + 
+			          vR.locV.y * math.cos(thRadN)
+				-- new location of received after rotation
+			paraT.x = robotsRT[vR.idS].locV.x - paraT.x
+			paraT.y = robotsRT[vR.idS].locV.y - paraT.y
+				-- translation vector
+			paraT.thN = thN
+		end
+	end
+
+	if paraT.thN == nil then
+		print("no overlapping robot, can't join")
+	else
+		local thN = paraT.thN
+		local thRadN = thN * math.pi / 180
+		local nRobots = #robotsRT
+		for i, vR in ipairs(receivedRobotsRT) do
+			-- calc and add into robotsRT
+			if robotsRT[vR.idS] == nil then
+				nRobots = nRobots + 1
+				robotsRT[nRobots] = {}
+				robotsRT[nRobots].locV = {
+					x = vR.locV.x * math.cos(thRadN) - 
+					    vR.locV.y * math.sin(thRadN) + paraT.x,
+					y = vR.locV.x * math.sin(thRadN) + 
+					    vR.locV.y * math.cos(thRadN) + paraT.y,
+				}
+				robotsRT[nRobots].idS = vR.idS
+				robotsRT[nRobots].parent = vR.parent
+				robotsRT[nRobots].dirN = (vR.dirN + thN) % 360  
+					-- it should still be inside range [0,360]
+				robotsRT[vR.idS] = robotsRT[nRobots]
+			end
+		end
+	end
+	return robotsRT, paraT
+end
+
+----------------------------------------------------------------------------------
+--   Lua Interface
+----------------------------------------------------------------------------------
+function setVelocity(x,y,theta)
+	robot.joints.axis0_axis1.set_target(x)
+	robot.joints.axis1_axis2.set_target(y)
+	robot.joints.axis2_body.set_target(theta)
+end
+
+function getLEDsT()
+	return robot.cameras.fixed_camera.led_detector
+end
+
+function getTagsT()
+	return robot.cameras.fixed_camera.tag_detector
+end
+
+function transData(xBT)		--BT means byte table
+	robot.radios["radio_0"].tx_data(xBT)
+end
+
+function getReceivedDataTableBT()
+	return robot.radios["radio_0"].rx_data
+end
+
+function getSelfIDS()
+	return robot.id
+end
---
+++ vehicle0.lua
@@ -0,0 +1,263 @@
+------------------------------------------------------------------------
+--   Global Variables
+------------------------------------------------------------------------
+
+require("PackageInterface")
+local State = require("StateMachine")
+
+--require("debugger")
+
+------------------------------------------------------------------------
+--   State Machine
+------------------------------------------------------------------------
+
+stateMachine = State:create{
+	data = {turnBySelfDir = nil},
+	initial = "randomWalk",
+	substates = 
+	{
+		randomWalk = State:create{
+			transMethod = function(fdata, data, para)
+				local fromidS, cmdS, rxNumbersNT = getCMD()
+				if cmdS == "recruit" then
+					return "beingDriven"
+				end
+
+				--[[
+				if objFront() == true then
+					if math.random() > 0.5 then fdata.turnBySelfDir = "left"
+					                       else fdata.turnBySelfDir = "right" end
+					return "turnBySelf"
+				end
+				--]]
+			end,
+			initial = "straight",
+			substates = {
+				straight = State:create{
+					enterMethod = function() goFront() end,
+					transMethod = function()
+						if objFront() == true then
+							standStill()
+							return "turn"
+						end
+						sideForward((math.random() - 0.5) * 5)
+					end,
+				}, 
+				turn = State:create{
+					enterMethod = function()
+						if math.random() > 0.5 then turnLeft()
+						                       else turnRight() end
+					end,
+					transMethod = function()
+						if objFront() == false then
+							return "straight"
+						end
+					end,
+				},
+			},
+		}, -- end of randomWalk
+		beingDriven = State:create{
+			data = {countN = 0},
+			enterMethod = function() setSpeed(0, 0) print(getSelfIDS(), ": I am recruited") end,
+			transMethod = function(fdata, data, para)
+				local fromidS, cmdS, rxNumbersNT = getCMD()
+				if cmdS == "setspeed" then
+					setSpeed(rxNumbersNT[1], rxNumbersNT[2])
+				end
+				if cmdS == "dismiss" then
+					print(getSelfIDS(), ": I am dismissed")
+					return "randomWalk"
+				end
+				if cmdS == "turn" then
+					if rxNumbersNT[1] == 1 then
+						fdata.turnBySelfDir = "left"
+					else
+						fdata.turnBySelfDir = "right"
+					end
+					return "turnBySelf"
+				end
+				if fromidS ~= nil then
+					sendCMD(fromidS, "sensor", getProximityTableNT())
+					data.countN = 0
+				else
+					-- i didn't get command when I should be
+					data.countN = data.countN + 1
+					if data.countN > 3 then
+						print(getSelfIDS(), ": I am lost")
+						return "randomWalk"
+					end
+				end
+			end,
+		}, -- end of beingDriven
+		turnBySelf = State:create{
+			data = {turnBySelfDir = nil},
+			enterMethod = function(fdata, data, para)
+				print(getSelfIDS(), "i am turnBySelf", fdata.turnBySelfDir)
+				data.turnBySelfDir = fdata.turnBySelfDir
+			end,
+			transMethod = function()
+				if objFront() == false then goFront() end 
+					-- make it walk along the box in the future
+					
+				local fromidS, cmdS, rxNumbersNT = getCMD()
+				if cmdS == "setspeed" then
+					setSpeed(rxNumbersNT[1], rxNumbersNT[2])
+					return "beingDriven"
+				end
+				if cmdS == "dismiss" then
+					print(getSelfIDS(), ": I am dismissed")
+					return "randomWalk"
+				end
+			end,
+			initial = "turn",
+			substates = {
+				turn = State:create{
+					enterMethod = function(fdata, data, para)
+						if fdata.turnBySelfDir == "left" then turnLeft()
+						                                 else turnRight() end
+					end,
+					transMethod = function()
+						if objFront() == false then return "walkAlong" end 
+					end,
+				},
+				walkAlong = State:create{
+					enterMethod = function() goFront() end,
+					transMethod = function(fdata, data, para)
+						--[[ it doesn't work
+						local testFunc, turnFunc
+						if fdata.turnBySelfDir == "left" then testFunc = objNearRight
+						                                      turnFunc = turnFrontRight
+						                                 else testFunc = objNearLeft 
+						                                      turnFunc = turnFrontLeft end
+						if testFunc() == false then turnFunc()
+						                       else goFront() end
+						--]]
+					end,
+				},
+			}, -- end of substates of turnBySelf
+		}, -- end of turnBySelf
+	} -- end of substates of stateMachine
+} -- end of stateMachine
+
+------------------------------------------------------------------------
+--   ARGoS Functions
+------------------------------------------------------------------------
+function init()
+	setTag(getSelfIDS())
+	reset()
+end
+
+-------------------------------------------------------------------
+function step()
+	if getSelfIDS() ~= "mark0" and 
+	   getSelfIDS() ~= "mark1" and
+	   getSelfIDS() ~= "mark2" then
+		stateMachine:step()
+	end
+end
+
+-------------------------------------------------------------------
+function reset()
+	math.randomseed(1)
+	-- TODO: get random seed from xml
+end
+
+-------------------------------------------------------------------
+function destroy()
+   -- put your code here
+end
+
+------------------------------------------------------------------------
+--   Customize Functions
+------------------------------------------------------------------------
+
+local baseSpeedN = 10
+function standStill()
+	setSpeed(0, 0)
+end
+function goFront()
+	setSpeed(baseSpeedN, baseSpeedN)
+end
+function turnLeft()
+	setSpeed(-baseSpeedN, baseSpeedN)
+end
+function turnFrontLeft()
+	setSpeed(0, baseSpeedN)
+end
+function turnRight()
+	setSpeed(baseSpeedN, -baseSpeedN)
+end
+function turnFrontRight()
+	setSpeed(baseSpeedN, 0)
+end
+function sideForward(x) -- 0 < x < 1
+	setSpeed(baseSpeedN - baseSpeedN * x, baseSpeedN + baseSpeedN * x)
+end
+
+-------------------------------------------------------------------
+-- Proximity sensors:  1 in front, 4 left, 7 back, 10 right
+function objFront()
+	if getProximityN(1) ~= 0 or
+	   getProximityN(2) ~= 0 or
+	   getProximityN(12) ~= 0 then
+		return true
+	else
+		return false
+	end
+end
+
+function objNearLeft()
+	if getProximityN(3) ~= 0 and
+	   getProximityN(4) ~= 0 and
+	   getProximityN(5) ~= 0 then return true
+	                         else return false end
+end
+
+function objFarLeft()
+	if getProximityN(4) ~= 0 then return true
+	                         else return false end
+end
+
+function objNearRight()
+	if getProximityN(10) ~= 0 then return true
+	                          else return false end
+end
+
+function objFarRight()
+	if getProximityN(9) ~= 0 or
+	   getProximityN(10) ~= 0 or
+	   getProximityN(11) ~= 0 then return true
+	                          else return false end
+end
+
+------------------------------------------------------------------------
+--   Lua Interface
+------------------------------------------------------------------------
+function setSpeed(x,y)
+	robot.joints.base_wheel_left.set_target(x)
+	robot.joints.base_wheel_right.set_target(-y)
+end
+
+function getProximityN(x)
+	return robot.proximity[x]
+end
+
+function getProximityTableNT()
+	return robot.proximity
+end
+
+function transData(xBT)
+	robot.radios["radio_0"].tx_data(xBT)
+end
+
+function getReceivedDataTableBT()	--BT means byte table
+	return robot.radios["radio_0"].rx_data
+end
+
+function getSelfIDS()
+	return robot.id
+end
+
+function setTag(str)
+	robot.tags.set_all_payloads(str)
+end
